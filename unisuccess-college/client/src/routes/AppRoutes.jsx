438950cffd42660375f7468bf42e928d1b34a5c3
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2026-06-03 17:25:29 +0100

    Update and rename i to AppRoutes.jsx

---
+++ unisuccess-college/client/src/routes/AppRoutes.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Programmes from "../pages/Programmes";
+import Admissions from "../pages/Admissions";
+import Contact from "../pages/Contact";
+
+import Login from "../pages/Login";
+import StudentPortal from "../pages/StudentPortal";
+import ResultChecker from "../pages/ResultChecker";
+import Dashboard from "../pages/Dashboard";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/programmes" element={<Programmes />} />
+        <Route path="/admissions" element={<Admissions />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/student-portal" element={<StudentPortal />} />
+        <Route path="/result-checker" element={<ResultChecker />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
