d8f73f58316182a9ba7e182a2f5fb766bed10b39
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2026-06-09 09:15:34 +0100

    Update App.jsx

--- unisuccess-college/client/src/App.jsx
+++ unisuccess-college/client/src/App.jsx
@@ -1,20 +1,7 @@
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-
-      <AppRoutes />
-
-      <Footer />
-
-      <WhatsAppButton />
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
